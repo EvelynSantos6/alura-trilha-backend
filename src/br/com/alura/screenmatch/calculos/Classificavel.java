fb359faf631fc705ad34e5c8b5dc9c685c561a81
EvelynSantos6 <evelyn.santos20032@gmail.com>
2026-03-16 10:30:40 -0300

    implementacao da interface

---
+++ src/br/com/alura/screenmatch/calculos/Classificavel.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.calculos;
+
+public interface Classificavel {
+    //quem usar o calssificavel precisa ter o getClassificacao.
+    int getClassificacao();
+}
---
+++ src/br/com/alura/screenmatch/model/Titulo.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.model;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //metodo que exibe a ficha tecnica do filme
+    public void exibeFichaTecnica () {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    //retorna a media dividindo a soma das avaliações pelo totoal de avaliações
+    public double pegarMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
