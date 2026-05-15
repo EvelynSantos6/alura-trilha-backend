a6be87aae8b7ae521cfff4ad9880bd6749377103
EvelynSantos6 <evelyn.santos20032@gmail.com>
2026-05-14 21:09:57 -0300

    Implementação do List

--- src/MainLista.java
+++ src/MainLista.java
@@ -1,22 +1,31 @@
 import br.com.alura.screenmatch.model.Filme;
 import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Titulo;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainLista {
 
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme("Crepusculo", 2006 );
+        meuFilme.avalia(10);
         var filmeEve = new Filme("Agente secreto", 2026);
+        filmeEve.avalia(7);
         Serie ahs = new Serie("American Horror Story", 2012);
+        ahs.avalia(10);
 
-        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
-        listaDeFilmes.add(filmeEve);
-        listaDeFilmes.add(meuFilme);
-        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
-        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
-        System.out.println("toString do" +
-                " filme: " + listaDeFilmes.get(0).toString());
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filmeEve);
+        lista.add(meuFilme);
+        lista.add(ahs);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            if(item instanceof Filme filme && filme.getClassificacao() >2) {
+                System.out.println("Classiicação: " + filme.getClassificacao());
+            }
+        }
     }
 }
